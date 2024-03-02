a4b21c2dbe620773ac4ce8859ea25ec2484eed94
09pipapipa <148035399+09pipapipa@users.noreply.github.com>
2024-03-02 13:04:14 +0900

    連結や計算

--- app/javascript/practice.js
+++ app/javascript/practice.js
@@ -1,7 +1,7 @@
-alert("ようこそ")
+//alert("ようこそ")
 /*alert(4+5)
 //コメントアウト練習
 alert(5-2);*/
-console.log("コメントアウト練習")
-alert("こんにちは");alert("さようなら");
+//console.log("コメントアウト練習")
+//alert("こんにちは");alert("さようなら");
 
