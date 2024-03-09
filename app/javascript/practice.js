6946b04d22ce6ec1108c89e53306d50c6d714cc9
09pipapipa <148035399+09pipapipa@users.noreply.github.com>
2024-03-09 17:31:49 +0900

    デジタルタイマーの作成

--- app/javascript/practice.js
+++ app/javascript/practice.js
@@ -1,3 +1,26 @@
-alert("ようこそ")
-alart(4+5)
-alert(5-2);
+let hourBox, minBox, secBox;
+let timer;
+
+function init() {
+  hourBox = document.clockForm.hour;
+  minBox = document.clockForm.minute;
+  secBox = document.clockForm.sec;
+  dClock();
+}
+
+function dClock() {
+  let now = new Date();
+  let hour = now.getHours();
+  let min = now.getMinutes();
+  let sec = now.getSeconds();
+
+  if (hour < 10 ) hour = "0" + hour;
+  if (min < 10 ) min = "0" + min;
+  if (sec < 10) sec = "0" + sec;
+  hourBox.value = hour;
+  minBox.value = min;
+  secBox.value = sec;
+  timer = setTimeout(dClock, 1000);
+}
+
+window.onload = init;
