e415767bf42e32ae2b6fcfe65e6e3c4494e1b45a
09pipapipa <148035399+09pipapipa@users.noreply.github.com>
2024-03-10 09:27:03 +0900

    運勢占いとjs読み込み

--- app/javascript/practice.js
+++ app/javascript/practice.js
@@ -1,3 +1,12 @@
-alert("ようこそ")
-alart(4+5)
-alert(5-2);
+let kuji = new Array(4);
+kuji[0] = "<h1 style='color:green'>大吉</h1>"
+kuji[1] =  "<h2>中吉</h2>"
+kuji[2] =  "<h3>小吉</h3>"
+kuji[3] = "<h4 style='color:red'>凶</h4>"
+
+
+function unsei() {
+  let myArea = document.getElementById("myArea")
+  let num = Math.floor(kuji.length * Math.random());
+  myArea.innerHTML = kuji[num];
+}
